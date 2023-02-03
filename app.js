5a20b1631cea9c8119de70fae19bf8df5f5dc82d
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-02-03 20:28:57 +0530

    update style.css

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
 
 function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
@@ -14,7 +14,7 @@ function errorHandler(error) {
 }
 
 function btnClick() {
-    var inputText = txtInput.vlaue; 
+    var inputText = txtInput.value; 
 
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
